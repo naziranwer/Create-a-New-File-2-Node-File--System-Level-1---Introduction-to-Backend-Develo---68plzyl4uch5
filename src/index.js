622cea8276bed41d7536edcbdbe6676f6e484395
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2023-12-29 12:41:47 +0530

    files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
     //Write your code here
     //Don't change function name
+    try{
+
+        const fileExists = await fs.access(fileName)
+            .then(() => true)
+            .catch(() => false);
+
+       if(fileExists){
+        return;
+       }
+       else {
+           await fs.writeFile(fileName,fileContent);
+        
+       }
+    }
+    catch(e){
+        console.error(e);
+    }
   
 };
 module.exports =  writeFile ;
